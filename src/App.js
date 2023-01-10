0c1d3f9e4d9e614ce0737f42e9ae053f0e938f51
Gia Madonna <giamadonna1@gmail.com>
2023-01-09 21:31:33 -0800

    Finished bootstrap. Super nice to use

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import './App.css';
-
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
 
 
 function App() {
   return (
     <div className="App">
+      <Nav activeKey="/home" fill variant='tabs'>
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='/about'>About</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <h1>Bootstrap Website</h1>
-     
+        <Button variant="danger">danger</Button>
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
